5e1c7a3b69890cb9b2f2cf41ed05f21967b96977
Tejaswi1506 <119721388+Tejaswi1506@users.noreply.github.com>
2023-08-18 15:07:53 +0530

    ch=input() if(ch=='V'):     print("Violet") elif(ch=='i'):     print("Indigo") elif(ch=='B'):     print("Blue") elif(ch=='G'):     print("Green") elif(ch=='Y'):     print("Yellow") elif(ch=='O'):     print("Orange") elif(ch=='R'):     print("Red") else:     print("-1")

---
+++ Print_the_color_name.py
@@ -0,0 +1,18 @@
+ch=input()
+if(ch=='V'):
+    print("Violet")
+elif(ch=='i'):
+    print("Indigo")
+elif(ch=='B'):
+    print("Blue")
+elif(ch=='G'):
+    print("Green")
+elif(ch=='Y'):
+    print("Yellow")
+elif(ch=='O'):
+    print("Orange")
+elif(ch=='R'):
+    print("Red")
+else:
+    print("-1")
+    
